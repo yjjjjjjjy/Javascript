8bfab8383fe601c176bfbabc053e225ad6823d98
yjjjjjjjy <yunjy0088@gmail.com>
2022-03-11 18:00:12 +0900

    commit

--- Momentum/js/greeting.js
+++ Momentum/js/greeting.js
@@ -2,6 +2,7 @@ const loginForm=document.querySelector("#login-form");
 const loginInput=document.querySelector("#login-form input");
 const greeting=document.querySelector("#greeting");
 const todoForm=document.querySelector("#todo-form");
+const todoList=document.querySelector("#todo-list");
 
 const HIDDEN_CLASSNAME="hidden";
 const USERNAME_KEY="username";
@@ -19,6 +20,7 @@ function paintGreetings(username) {
     greeting.innerText=`Hello ${username} !`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
     todoForm.classList.remove(HIDDEN_CLASSNAME);
+    todoList.classList.remove(HIDDEN_CLASSNAME);
 }
 
 //"username" key값의 value값을 savedUsername에 저장
--- Momentum/js/todo.js
+++ Momentum/js/todo.js
@@ -36,7 +36,7 @@ function paintToDo(newTodo) {
     const span=document.createElement("span");
     span.innerText=newTodo.text;
     const button=document.createElement("button");
-    button.innerText="X";
+    button.innerHTML="<i class='far fa-trash-alt'></i>";
     button.addEventListener("click", deleteToDo);
     li.appendChild(span);
     li.appendChild(button);
